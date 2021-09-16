fb841727e5754d57b9d6189424ca55c4d3ea1781
indriyanto Nugroho <indriyantongrhh@gmail.com>
2021-09-16 20:35:29 +0700

    initial commit

---
+++ src/containers/TheFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <CFooter :fixed="false">
+    <!-- <div>
+
+      <span class="ml-1">&copy; {{new Date().getFullYear()}} PT. Bank Negara Indonesia.</span>
+    </div> -->
+    <div class="mfs-auto">
+    <span class="ml-1">&copy; {{new Date().getFullYear()}} PT. Bank Negara Indonesia .Tbk </span>
+    </div>
+  </CFooter>
+</template>
+
+<script>
+export default {
+  name: 'TheFooter'
+}
+</script>
